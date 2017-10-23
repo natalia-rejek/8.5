d22af1a6934f6f73bed7eb42599bf7ce8327efdb
Natalia <nettel@wp.pl>
2017-10-23 12:38:09 +0200

    I addded all the files to the new branch

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+// scripts.js
+
+var triangleArea
+	h = 4,
+	a = 3;
+triangleArea = a*h/2,
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
